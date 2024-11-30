d878da0725a8e36a51c2a0f87a832362ffa78173
Tsumaki <matsu.yukii22@gmail.com>
2024-12-01 02:56:12 +0800

    Finished adding general settings for editing the General Settings of the Hotel, Added Maintenance Switch settings for maintenance on the website, added editing the contact settings of the website and also for adding and removing the members of the Management Team.

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -1,5 +1,15 @@
 <?php 
 
+  //Frontend purpose
+
+  define('SITE_URL','http://127.0.0.1/hotelreservationsystem/');
+  define('ABOUT_IMG_PATH',SITE_URL.'images/About/');
+
+  //Backend upload process
+  
+  define('UPLOAD_IMAGE_PATH',$_SERVER['DOCUMENT_ROOT'].'/hotelreservationsystem/images/');
+  define('ABOUT_FOLDER','About/');
+
   function adminLogin()
   {
     session_start();
@@ -7,14 +17,15 @@
       echo"<script>
       window.location.href='index.php'
       </script>";
+      exit;
     }
-    session_regenerate_id(true);
   }
 
   function redirect($url){
     echo"<script>
       window.location.href='$url';
       </script>";
+      exit;
   }
 
   function alert($type,$msg){
@@ -29,9 +40,38 @@
     alert;
   }
 
+  function uploadImage($image,$folder)
+  {
+    $valid_mime = ['image/jpeg','image/png','image/webp'];
+    $img_mime = $image['type'];
 
+    if(!in_array($img_mime,$valid_mime)){
+      return ' inv_img'; //Invalid image mime or format
+    }
+    else if(($image['size']/(1024*1024))>2){
+      return 'inv_size'; //Invalid size greater than 2MB
+    }
+    else{
+      $ext = pathinfo($image['name'],PATHINFO_EXTENSION);
+      $rname = 'IMG_'.random_int(11111,99999).".$ext";
 
+      $img_path = UPLOAD_IMAGE_PATH.$folder.$rname;
+      if(move_uploaded_file($image['tmp_name'],$img_path)){
+        return $rname;
+      }
+      else{
+        return 'upd_failed';
+      }
+    }
+  }
 
-
-
+  function deleteImage($image,$folder)
+  {
+    if(unlink(UPLOAD_IMAGE_PATH.$folder.$image)){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
 ?>
